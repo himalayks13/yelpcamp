c8eaa5275bfa2e75e2c02f80be48537db3ba9cc0
“himalayks13” <1rn20cs053@RNSIT,onmicrosoft.com>
2024-05-28 18:50:48 +0530

    Yelpcamp website

---
+++ routes/users.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const catchAsync = require('../utils/catchAsync');
+const User = require('../models/user');
+const users = require('../controllers/users');
+const { storeReturnTo } = require('../middleware');
+
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
+
+router.route('/login')
+    .get(users.renderLogin)
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
+
+
+
+//router.get('/logout', users.logout)
+router.get('/logout', (req, res, next) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success', 'Goodbye!');
+        res.redirect('/campgrounds');
+    });
+});
+router.post('/login',
+    storeReturnTo,
+    passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }),
+    (req, res) => {
+        req.flash('success', 'Welcome back!');
+        const redirectUrl = res.locals.returnTo || '/campgrounds';
+        res.redirect(redirectUrl);
+    });
+
+
+module.exports = router;
